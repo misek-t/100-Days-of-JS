bf19f268eb04a69004c5a38317b3830773d70127
misek <120718679+misek-t@users.noreply.github.com>
2023-05-22 01:38:12 +0800

    Update script.js

--- day8/script.js
+++ day8/script.js
@@ -7,9 +7,13 @@ var back1 = document.getElementById("back1");
 var back2 = document.getElementById("back2");
 var progress = document.getElementById("progress");
 
+// onclick event handler - when the next button is clicked, the following code will be executed
 next1.onclick = function () {
+    // when next button is clicked, form1 will move to the left and will be hid from view
     form1.style.left = "-450px";
+    // form2 will then move 40px to the left making it visible to the viewer
     form2.style.left = "40px";
+    // change the width of the progress to 240px
     progress.style.width = "240px";
 }
 
